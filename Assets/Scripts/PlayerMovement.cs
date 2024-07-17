90e22356491be80b124b4c083e1e162fa611cded
YoavLippi <131270199+YoavLippi@users.noreply.github.com>
2024-07-17 21:16:07 +0200

    Set up Inputs and basic character controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables
+
+        [Header("Setup")] 
+        [SerializeField] private PlayerInput pInput;
+        [SerializeField] private Rigidbody2D rb;
+
+        [Header("Character Physics")] 
+        [SerializeField] private Vector2 currentMovement;
+        [SerializeField] private float moveSpeed;
+
+    #endregion
+
+    // Start is called before the first frame update
+
+    private void OnEnable()
+    {
+        pInput.actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        pInput.actions.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        DoPlayerMovement();
+    }
+
+    private void DoPlayerMovement()
+    {
+        rb.MovePosition(rb.position+currentMovement*(moveSpeed*Time.deltaTime));
+    }
+
+    private void OnMove()
+    {
+        currentMovement = pInput.actions.FindAction("Move").ReadValue<Vector2>();
+        Debug.Log("Test");
+    }
+}
