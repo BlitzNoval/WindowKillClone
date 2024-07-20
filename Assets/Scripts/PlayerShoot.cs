ea71487536b5468846833a777a3f468af2ba79b4
JayQkew <rayjebb@gmail.com>
2024-07-20 11:14:19 +0200

    InputManager > InputSystem

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,20 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerShoot : MonoBehaviour
 {
-    public GameObject p_bullet;
-    public GameObject p_point;
-    public Transform shootPoint_parent;
-    public List<GameObject> bulletSpawnPos;
+    #region Variables
+    [SerializeField] private PlayerInput pInput;
+
+    [SerializeField] private GameObject p_bullet;
+    [SerializeField] private GameObject p_point;
+    [SerializeField] private Transform shootPoint_parent;
+    [SerializeField] private List<GameObject> bulletSpawnPos;
 
     public float bulletSpeed;
-    public float radius = 0.5f;
-    public float a;
+    [SerializeField] private float radius = 0.5f;
+    [SerializeField] private float startAngle;
+    [SerializeField] private float angleSpread = 90f;
+    #endregion
+
+    private void OnEnable()
+    {
+        pInput.actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        pInput.actions.Disable();
+    }
 
     private void Start()
     {
+        startAngle = 90f - (angleSpread / 2f);
+
         GetShootPositions();
         SortShootPos();
 
@@ -26,7 +44,9 @@ public class PlayerShoot : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) Shoot();
+        //new input system
+        if (pInput.actions.FindAction("Fire").WasPressedThisFrame()) Shoot();
+        //if (Input.GetMouseButtonDown(0)) Shoot();
         if (Input.GetKeyDown(KeyCode.P)) Upgrade_MulltiShot();
         AimDirection();
     }
@@ -53,8 +73,8 @@ public class PlayerShoot : MonoBehaviour
     //gets active shoot positions
     private void GetShootPositions()
     {
-        List<GameObject> newShotPos = new List<GameObject>(); 
-        for (int i = 0;i < shootPoint_parent.childCount; i++)
+        List<GameObject> newShotPos = new List<GameObject>();
+        for (int i = 0; i < shootPoint_parent.childCount; i++)
         {
             newShotPos.Add(shootPoint_parent.GetChild(i).gameObject);
         }
@@ -66,8 +86,8 @@ public class PlayerShoot : MonoBehaviour
     private void ShootPointPos(GameObject point, float angle)
     {
         Vector2 newPos;
-        newPos.x = shootPoint_parent.position.x + (radius * Mathf.Cos(angle / (180f / Mathf.PI)));
-        newPos.y = shootPoint_parent.position.y + (radius * Mathf.Sin(angle / (180f / Mathf.PI)));
+        newPos.x = shootPoint_parent.localPosition.x + (radius * Mathf.Cos(angle / (180f / Mathf.PI)));
+        newPos.y = shootPoint_parent.localPosition.y + (radius * Mathf.Sin(angle / (180f / Mathf.PI)));
 
         point.transform.localPosition = newPos;
 
@@ -76,11 +96,10 @@ public class PlayerShoot : MonoBehaviour
     //sorts each shooting position evenly
     private void SortShootPos()
     {
-        float angle = 90 / (bulletSpawnPos.Count + 1);
+        float angle = angleSpread / (bulletSpawnPos.Count + 1);
         for (int i = 0; i < bulletSpawnPos.Count; i++)
         {
-            ShootPointPos(bulletSpawnPos[i], (angle * (i + 1)) + a);
-
+            ShootPointPos(bulletSpawnPos[i], (angle * (i + 1)) + startAngle);
         }
     }
 
@@ -95,7 +114,8 @@ public class PlayerShoot : MonoBehaviour
     //lets the player aim in the direction of thier mouse
     private void AimDirection()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Vector2 mousePos = pInput.actions.FindAction("Look").ReadValue<Vector2>();
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(pInput.actions.FindAction("Look").ReadValue<Vector2>());
         Vector2 lookDir = mousePos - (Vector2)shootPoint_parent.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         Quaternion rotation = Quaternion.Euler(0, 0, angle);
