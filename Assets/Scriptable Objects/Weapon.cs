5421969cf4e4c4d0ceb180fcef7dee1e6c7a1850
YoavLippi <131270199+YoavLippi@users.noreply.github.com>
2024-07-21 23:08:00 +0200

    Added playerstats and weapontype enums Added data for weapons

--- Assets/Scriptable Objects/Weapon.cs
+++ Assets/Scriptable Objects/Weapon.cs
@@ -8,6 +8,44 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Weapon : ScriptableObject
 {
-    [SerializeField] private WeaponTier weaponTier;
+    [Serializable]
+    public struct StatScaling
+    {
+        //Using the multiplier value. ie: 0.8 = 80% scaling
+        [SerializeField] private float scalingAmount;
+        [SerializeField] private PlayerStats scalingType;
+        public float ScalingAmount
+        {
+            get => scalingAmount;
+            set => scalingAmount = value;
+        }
+        public PlayerStats ScalingType
+        {
+            get => scalingType;
+            set => scalingType = value;
+        }
+    }
+    //Wrapper class to add multiple scaling types for serialization
+    [Serializable]
+    public struct StatScalingTier
+    {
+        [SerializeField] private StatScaling[] scalings;
+        public StatScaling[] Scalings
+        {
+            get => scalings;
+            set => scalings = value;
+        }
+    }
+    [SerializeField] private WeaponType weaponType;
     [SerializeField] private WeaponClass weaponClass;
+    [SerializeField] private WeaponTier weaponTier;
+    [SerializeField] private float[] damagePerTier;
+    [SerializeField] private StatScalingTier[] scalingPerTier;
+    [SerializeField] private float[] attackSpeedPerTier;
+    [SerializeField] private float[] critDamagePerTier;
+    [SerializeField] private float[] critChancePerTier;
+    [SerializeField] private float[] rangePerTier;
+    [SerializeField] private float[] knockbackPerTier;
+    [SerializeField] private float[] lifestealPerTier;
+    [SerializeField] private int[] basePricePerTier;
 }
