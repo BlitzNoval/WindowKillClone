c04dc77c10922196f18c6a2d727267540652fb04
JayQkew <rayjebb@gmail.com>
2024-07-22 17:33:51 +0200

    creating upgrade arrays

--- Assets/Scripts/PlayerUpgrades.cs
+++ Assets/Scripts/PlayerUpgrades.cs
@@ -4,4 +4,21 @@ using UnityEngine;
 
 public class PlayerUpgrades : MonoBehaviour
 {
+    public static PlayerUpgrades Instance { get; private set; }
+
+    public Upgrades[] Heart;
+    public Upgrades[] Lungs;
+    public Upgrades[] Teeth;
+    public Upgrades[] Triceps;
+    public Upgrades[] Forearms;
+    public Upgrades[] Shoulders;
+    public Upgrades[] Brain;
+    public Upgrades[] Reflexes;
+    public Upgrades[] Fingers;
+    public Upgrades[] Skull;
+    public Upgrades[] Eyes;
+    public Upgrades[] Chest;
+    public Upgrades[] Back;
+    public Upgrades[] Legs;
+    public Upgrades[] Nose;
 }
