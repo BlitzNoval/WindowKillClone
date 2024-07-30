0694a1d4cb8e5bfa53aa3c4f5bbb3a9305a27fe5
ljmoo <131270241+BlitzNoval@users.noreply.github.com>
2024-07-30 22:41:27 +0200

    Enemy Assets Added

--- Assets/Scripts/Game Manager/GameManager.cs
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -48,7 +48,7 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0f;
         if (player != null)
         {
-            player.GetComponent<PlayerMovement>().enabled = false; // Assuming the player has a PlayerController script
+            player.GetComponent<PlayerMovement>().enabled = false; // Stops the player from moving 
         }
     }
 
