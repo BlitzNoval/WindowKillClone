8ff353cb9406c857d653569969c2f213fb6c13ec
JayQkew <rayjebb@gmail.com>
2024-08-02 18:08:05 +0200

    almost final stuff

--- Assets/Scripts/Weapons/Template/ProjectileBehaviour.cs
+++ Assets/Scripts/Weapons/Template/ProjectileBehaviour.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -9,19 +7,20 @@ public class ProjectileBehaviour : MonoBehaviour
 {
     private Vector2 startingPostion;
     public float damage;
-    //The distance the projectile will travel until it times out
+    // The distance the projectile will travel until it times out
     public float maxRange;
     public int maxPierce;
 
     private int currentPierce;
-    //The distance the projectile has travelled
+    // The distance the projectile has travelled
     [SerializeField] private float currentRange;
     public WeaponBehaviour parentWeapon;
+    private float lifetime = 5f; // Time in seconds before the projectile despawns
 
     private void FixedUpdate()
     {
         currentRange = Vector2.Distance(startingPostion, transform.position);
-        //Range checks
+        // Range checks
         if (currentRange >= maxRange)
         {
             Destroy(this.gameObject);
@@ -37,14 +36,21 @@ public class ProjectileBehaviour : MonoBehaviour
         currentPierce = maxPierce;
         currentRange = maxRange;
         startingPostion = transform.position;
+
+        StartCoroutine(DestroyAfterTime(lifetime)); // Start the despawn timer
     }
 
+    private IEnumerator DestroyAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
-            //The bullet has hit an enemy
+            // The bullet has hit an enemy
             other.gameObject.GetComponent<Enemy>().TakeDamage(damage);
             currentPierce--;
             if (currentPierce < 0)
